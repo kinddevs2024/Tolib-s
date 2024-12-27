3c0661667eaac5d3c2a6164c974ca8419718dde1
kinddevs <kinddevs2024@gmail.com>
2024-12-27 20:30:24 +0500

    start

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import {  } from 'react'
+import { Route, Routes ,  } from 'react-router-dom'
+import './App.css'
+import Loyaout from './Layout/Loyaout'
+import Home from './pages/Home/Home'
+import Aboute from './pages/About/Aboute'
+import { useState, useEffect } from "react";
+import Eror from './pages/Eror-404/Eror'
+
+function App() {
+  const [theme, setTheme] = useState(() => {
+		const initialTheme = localStorage.getItem("theme");
+		return initialTheme ? initialTheme : "light";
+	});
+  return (
+    <>
+    <Loyaout >
+      <Routes >
+        <Route  path='/' element={<Home />}/>
+        <Route path='*' element={ <Eror/>}/>
+      </Routes>
+    </Loyaout>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+export function Home() {
+ 
+  return (
+    <div className="grid container  text-black dark:text-white   m-auto mt-10 mb-10 grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+      
+      
+    </div>
+  );
+}
+ 
+
+
+export default Home
